b655aa88f6db84691bb89f97f171103b6c4049b6
unknown <akstr17coding@gmail.com>
2020-11-16 01:03:43 +0530

    typing

---
+++ js/typing.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var typed = new Typed(".typing", {
+        strings: ["Designer", "Tester", "Gamer", "Manager"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+});
